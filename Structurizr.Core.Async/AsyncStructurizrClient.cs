1ae54b94998ce9e571ff8a057300125d5a55b0a3
Gerhard Draser <gerhard.draser@mitutoyo-ctl.de>
2016-11-15 12:21:29 +0100

    Implemented AsyncStructurizrClient using synchronous client combined with the async API of the WebClient. Added HttpHeadersHelper to expose the AddHeaders method

--- Structurizr.Core.Async/AsyncStructurizrClient.cs
+++ Structurizr.Core.Async/AsyncStructurizrClient.cs
@@ -1,44 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using Structurizr.Client;
 using Structurizr.Encryption;
+using Structurizr.IO.Json;
 
 namespace Structurizr.Core.Async
 {
     public class AsyncStructurizrClient
     {
-        private readonly StructurizrClient _privateClient;
 
-        public string Url
-        {
-            get { return _privateClient.Url; }
-            set { _privateClient.Url = value; }
-        }
+        private const string WorkspacePath = "/workspace/";
 
-        public string ApiKey
-        {
-            get { return _privateClient.ApiKey; }
-            set { _privateClient.ApiKey = value; }
-        }
-
-        public string ApiSecret
-        {
-            get { return _privateClient.ApiSecret; }
-            set { _privateClient.ApiSecret = value; }
-        }
+        public string Url { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
 
         /// <summary>the location where a copy of the workspace will be archived when it is retrieved from the server</summary>
-        public DirectoryInfo WorkspaceArchiveLocation
-        {
-            get { return _privateClient.WorkspaceArchiveLocation; }
-            set { _privateClient.WorkspaceArchiveLocation = value; }
-        }
+        public DirectoryInfo WorkspaceArchiveLocation { get; set; }
 
-        public EncryptionStrategy EncryptionStrategy
-        {
-            get { return _privateClient.EncryptionStrategy; }
-            set { _privateClient.EncryptionStrategy = value; }
-        }
+        public EncryptionStrategy EncryptionStrategy { get; set; }
 
         public AsyncStructurizrClient(string apiKey, string apiSecret)
             : this("https://api.structurizr.com", apiKey, apiSecret)
@@ -47,17 +29,93 @@ namespace Structurizr.Core.Async
 
         public AsyncStructurizrClient(string apiUrl, string apiKey, string apiSecret)
         {
-            _privateClient = new StructurizrClient(apiUrl, apiKey, apiSecret);
+            this.Url = apiUrl;
+            this.ApiKey = apiKey;
+            this.ApiSecret = apiSecret;
+
+            this.WorkspaceArchiveLocation = new DirectoryInfo(".");
         }
 
-        public Task<Workspace> GetWorkspaceAsync(long workspaceId)
+        public async Task<Workspace> GetWorkspaceAsync(long workspaceId)
         {
-            return Task.Run(() => _privateClient.GetWorkspace(workspaceId));
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    string httpMethod = "GET";
+                    string path = WorkspacePath + workspaceId;
+
+                    HttpHeadersHelper.AddHeaders(webClient, httpMethod, path, "", "", ApiSecret, ApiKey);
+
+                    string response = await webClient.DownloadStringTaskAsync(this.Url + path);
+                    ArchiveWorkspace(workspaceId, response);
+
+                    StringReader stringReader = new StringReader(response);
+                    if (EncryptionStrategy == null)
+                    {
+                        return new JsonReader().Read(stringReader);
+                    }
+                    else
+                    {
+                        EncryptedWorkspace encryptedWorkspace = new EncryptedJsonReader().Read(stringReader);
+                        encryptedWorkspace.EncryptionStrategy.Passphrase = this.EncryptionStrategy.Passphrase;
+                        return encryptedWorkspace.Workspace;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new StructurizrClientException("There was an error getting the workspace: " + e.Message, e);
+                }
+            }
         }
 
-        public Task PutWorkspaceAsync(long workspaceId, Workspace workspace)
+        public async Task PutWorkspaceAsync(long workspaceId, Workspace workspace)
         {
-            return Task.Run(() => _privateClient.PutWorkspace(workspaceId, workspace));
+            workspace.Id = workspaceId;
+
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    string httpMethod = "PUT";
+                    string path = WorkspacePath + workspaceId;
+                    string workspaceAsJson = "";
+
+                    using (StringWriter stringWriter = new StringWriter())
+                    {
+                        if (EncryptionStrategy == null)
+                        {
+                            JsonWriter jsonWriter = new JsonWriter(false);
+                            jsonWriter.Write(workspace, stringWriter);
+                        }
+                        else
+                        {
+                            EncryptedWorkspace encryptedWorkspace = new EncryptedWorkspace(workspace, EncryptionStrategy);
+                            EncryptedJsonWriter jsonWriter = new EncryptedJsonWriter(false);
+                            jsonWriter.Write(encryptedWorkspace, stringWriter);
+                        }
+                        stringWriter.Flush();
+                        workspaceAsJson = stringWriter.ToString();
+                        System.Console.WriteLine(workspaceAsJson);
+                    }
+
+                    HttpHeadersHelper.AddHeaders(webClient, httpMethod, path, workspaceAsJson, "application/json; charset=UTF-8", ApiSecret, ApiKey);
+
+                    string response = await webClient.UploadStringTaskAsync(this.Url + path, httpMethod, workspaceAsJson);
+                    System.Console.WriteLine(response);
+                }
+                catch (Exception e)
+                {
+                    throw new StructurizrClientException("There was an error putting the workspace: " + e.Message, e);
+                }
+            }
+        }
+
+        public async Task MergeWorkspaceAsync(long workspaceId, Workspace workspace)
+        {
+            Workspace currentWorkspace = await GetWorkspaceAsync(workspaceId);
+            var mergedWorkspace = MergeWorkspaces(currentWorkspace, workspace);
+            await PutWorkspaceAsync(workspaceId, mergedWorkspace);
         }
 
         public Workspace MergeWorkspaces(Workspace currentWorkspace, Workspace newWorkspace)
@@ -67,6 +125,21 @@ namespace Structurizr.Core.Async
                 newWorkspace.Views.CopyLayoutInformationFrom(currentWorkspace.Views);
             }
             return newWorkspace;
+        }
+
+        private void ArchiveWorkspace(long workspaceId, string workspaceAsJson)
+        {
+            if (WorkspaceArchiveLocation != null)
+            {
+                File.WriteAllText(CreateArchiveFileName(workspaceId), workspaceAsJson);
+            }
+        }
+
+        private string CreateArchiveFileName(long workspaceId)
+        {
+            return Path.Combine(
+                WorkspaceArchiveLocation.FullName,
+                "structurizr-" + workspaceId + "-" + DateTime.UtcNow.ToString("yyyyMMddHHmmss") + ".json");
         }
     }
 }
